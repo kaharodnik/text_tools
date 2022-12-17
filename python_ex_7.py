d4ebc05e7c12f799da939530d95047af6dbe371b
Katsiaryna Aharodnik <kaharodnik@gradcenter.cuny.edu>
2022-12-17 15:17:01 -0500

    python scripts and text files

---
+++ python_ex_7.py
@@ -0,0 +1,18 @@
+#counting number of words for files with .txt in a directory
+import os
+
+def count_words(filename):
+    with open(filename, 'r') as f:
+        contents = f.readlines()
+        words = contents.split()
+        num_words = len(words)
+        num_types = len(set(words))
+        print(f"the file {filename} has about {num_words} words and {num_types} unique words.")
+
+
+dir = 'home/aharodnikk/gutenberg'
+for f in os.listdir(dir):
+    if f.endswith('.txt'):
+        print(f)
+        count_words(f)
+
